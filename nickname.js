02dc9d6848493de6dfba2435d8a2e6d2cc7a415b
lia <96490782+lia1324@users.noreply.github.com>
2022-06-01 18:19:41 +0900

    Create nickname.js

---
+++ nickname.js
@@ -0,0 +1,29 @@
+const { Client, Intents, Collection } = require('discord.js')
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.
+    FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES,],
+})
+
+const { token, prefix } = require('./config.json');
+
+client.commands = new Collection()
+
+client.once('ready', () => {
+  console.log(`로그인 완료 | ${client.user.tag}!`)
+})
+
+
+client.on('message', async message => {
+  if (message.content === '핑') return message.reply('퐁')
+  await DokdoHandler.run(message)
+})
+
+client.on('guildMemberAdd', member => {
+ member.setNickname(`고유번호 | ${member.user.username} | 직업`);
+  try {
+  } catch (error) {
+    return console.error(error)
+  }
+});
+
+client.login("")
